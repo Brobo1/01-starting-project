bdabb859459a470d86ea5fde6fa07f71fc2c70e1
brobo1 <armanahz95@gmail.com>
2024-02-29 22:51:47 +0100

    Refactor code styling for better readability
    The code has been tidied up to improve readability across the application. Main changes include reformatting component codes to maintain consistent spacing and indentation, removing unnecessary white lines, and better aligning lines for elements and attributes. It enhances code clarity and maintainability.

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -1,56 +1,61 @@
-import TabButton from "./TabButton";
+import TabButton  from "./TabButton";
 import {EXAMPLES} from "../data";
 import {useState} from "react";
-import {Section} from "./Section";
-import Tabs from "./Tabs";
+import {Section}  from "./Section";
+import Tabs       from "./Tabs";
 
 export function Examples() {
-	const [selected, setSelected] = useState();
-
-	function handleSelect(selectedBtn) {
-		setSelected(selectedBtn);
-	}
-
-	let tabContent = <p>Please select a topic</p>
-	if (selected) {
-		tabContent = (
-			<div id={"tab-content"}>
-				<h3>{EXAMPLES[selected].title}</h3>
-				<p>{EXAMPLES[selected].description}</p>
-				<pre>{EXAMPLES[selected].code}</pre>
-			</div>)
-
-	}
-	return (
-		<Section title={"Examples"} id={"examples"}>
-			<Tabs
-				buttons={
-					<>
-						<TabButton isSelected={selected === "components"}
-						           onClick={() => handleSelect("components")}
-						>
-							Components
-						</TabButton>
-						<TabButton isSelected={selected === "jsx"}
-						           onClick={() => handleSelect("jsx")}
-						>
-							JSX
-						</TabButton>
-						<TabButton isSelected={selected === "props"}
-						           onClick={() => handleSelect("props")}
-						>
-							Props
-						</TabButton>
-						<TabButton isSelected={selected === "state"}
-						           onClick={() => handleSelect("state")}
-						>
-							State
-						</TabButton>
-					</>
-				}>
-				{tabContent}
-
-			</Tabs>
-		</Section>
-	)
+  const [selected, setSelected] = useState();
+  
+  function handleSelect(selectedBtn) {
+	setSelected(selectedBtn);
+  }
+  
+  let tabContent = <p>Please select a topic</p>
+  if (selected) {
+	tabContent = (
+	  <div id={"tab-content"}>
+		<h3>{EXAMPLES[selected].title}</h3>
+		<p>{EXAMPLES[selected].description}</p>
+		<pre>{EXAMPLES[selected].code}</pre>
+	  </div>)
+	
+  }
+  return (
+	<Section title={"Examples"} id={"examples"}>
+	  <Tabs
+		buttons={
+		  <>
+			<TabButton
+			  isSelected={selected === "components"}
+			  onClick={() => handleSelect("components")}
+			>
+			  Components
+			</TabButton>
+			<TabButton
+			  isSelected={selected === "jsx"}
+			  onClick={() => handleSelect("jsx")}
+			>
+			  JSX
+			</TabButton>
+			<TabButton
+			  isSelected={selected === "props"}
+			  onClick={() => handleSelect("props")}
+			>
+			  Props
+			</TabButton>
+			<TabButton
+			  isSelected={selected === "state"}
+			  onClick={() => handleSelect("state")}
+			>
+			  State
+			</TabButton>
+		  </>
+		}
+	  >
+		{tabContent}
+	  
+	  </Tabs>
+	</Section>
+  )
 }
